2f6e42197c814eb9869ecdc76fb8e82e975fa8ea
Harry Brundage <harry.brundage@gmail.com>
2011-02-03 16:06:16 -0500

    Added proper repo support

--- blog.rb
+++ blog.rb
@@ -5,4 +5,20 @@ module Toto
       config[:to_html].call(path, page, self, blk)
     end
   end
+
+  class Repo
+
+    FILE = "https://github.com/%s/%s/raw/master/%s"
+
+    def readme
+      filename = (FILE % [@config[:github][:user], self[:name], @config[:github][:files][self[:name]]])
+      Tilt.new(filename) {open(filename).read }.render(self)
+    rescue Timeout::Error, OpenURI::HTTPError => e
+      homepage = "https://github.com/%s/%s" % [@config[:github][:user], self[:name]]
+      "Couldn't fetch the readme, you can try going to <a href=\"#{homepage}\">the project homepage</a>."
+    end
+
+    alias :content readme    
+  end
+
 end
